e40af8c3df7e58b34ca1120af4db3959ea40aa27
Alexander Selyutin <cool.selutin99@yandex.ru>
2019-03-25 21:35:25 +0400

    power point api

---
+++ DocumentCreator/DocumentCreator/FilesAPI/PowerPointAPI.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.Office.Core;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+
+namespace DocumentCreator.FilesAPI
+{
+    class PowerPointAPI
+    {
+        private static PowerPoint.Application app = new PowerPoint.Application();
+
+        public static PowerPoint.Presentation GetPresentation(string fileName)
+        {
+            //app.Visible = true;
+
+            PowerPoint.Presentation pre = null;
+
+            try
+            {
+                pre = app.Presentations.Open(fileName);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Can't open file", e);
+            }
+            return pre;
+        }
+
+        public static PowerPoint.Application GetPowerPointApp()
+        {
+            return app;
+        }
+
+        public static void SaveFile(PowerPoint.Presentation pre, string fileName = "")
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                try
+                {
+                    pre.Save();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Can't save file", e);
+                }
+            }
+            else
+            {
+                try
+                {
+                    pre.SaveAs(fileName);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Can't save file in " + fileName, e);
+                }
+            }
+        }
+
+        public static void Close(PowerPoint.Presentation pre)
+        {
+            if (pre != null)
+            {
+                pre.Close();
+                KillPowerPoint();
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+        }
+
+        public static void KillPowerPoint()
+        {
+            System.Diagnostics.Process[] procs = System.Diagnostics.Process.GetProcessesByName("POWERPNT");
+            foreach (System.Diagnostics.Process p in procs)
+            {
+                p.Kill();
+            }
+        }
+    }
+}
